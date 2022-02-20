2f7e329d27da95a833adceaa77322fbf30e9e1b7
Robbie Prokop <robbieprokop@gmail.com>
2022-02-20 02:11:39 +0000

    looping through arrays pratice

--- challenges.js
+++ challenges.js
@@ -79,3 +79,75 @@ const finalPosition = function (moves) {
   return [positionX, positionY];
 };
 console.log(finalPosition(moves));
+
+// AGE CALCULATOR
+
+const ageCalculator = function (name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth;
+  return name + " is " + age + " years old.";
+};
+
+console.log(ageCalculator("Suzie", 1983, 2015));
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+// HOW MANY HUNDREDS?
+
+const howManyHundreds = function (bottles) {
+  let containers = 0;
+  if (bottles / 100 < 1) {
+    return containers;
+  } else if (bottles / 100 >= 1) {
+    if (bottles % 100 === 0) {
+      containers += bottles / 100;
+    } else if (bottles % 100 != 0) {
+      containers += Math.floor(bottles / 100);
+    }
+  }
+  return containers;
+};
+// console.log(howManyHundreds(1000));
+// console.log(howManyHundreds(894));
+// console.log(howManyHundreds(520));
+// console.log(howManyHundreds(99));
+// console.log(howManyHundreds(0));
+
+// AREA CALCULATORS
+
+const calculateRectangleArea = function (length, width) {
+  const rectangle = length * width;
+  if (length < 0 || width < 0) {
+    return undefined;
+  }
+  return rectangle;
+};
+
+const calculateTriangleArea = function (base, height) {
+  const triangle = (base * height) / 2;
+  if (base < 0 || height < 0) {
+    return undefined;
+  }
+  return triangle;
+};
+
+const calculateCircleArea = function (radius) {
+  const circle = Math.PI * (radius * radius);
+  if (radius < 0) {
+    return undefined;
+  }
+  return circle;
+};
+
+console.log(calculateRectangleArea(10, 5));
+console.log(calculateRectangleArea(1.5, 2.5));
+console.log(calculateRectangleArea(10, -5));
+
+console.log(calculateTriangleArea(10, 5));
+console.log(calculateTriangleArea(3, 2.5));
+console.log(calculateTriangleArea(10, -5));
+
+console.log(calculateCircleArea(10));
+console.log(calculateCircleArea(3.5));
+console.log(calculateCircleArea(-1));
+//62.83185307179586
+//21.991148575128552
--- tinkering/tinkering.js
+++ tinkering/tinkering.js
@@ -540,3 +540,228 @@ const sayHello = function (name) {
 
 let greeting = sayHello("Robbie");
 console.log(greeting);
+
+// ARRAYS
+
+let donuts = ["glazed", "maple", "chocolate", "jelly"];
+let mixedData = ["abcd", 1, true, undefined, null, "everything!"];
+let nestedArray = [
+  [1, 2, 3],
+  ["Hello", "World"],
+  [true, false, true, true],
+];
+console.log(donuts);
+console.log(donuts[0]);
+
+donuts[1] = "glazed cruller";
+console.log(donuts[1]);
+
+let udaciFamily = ["Julia", "James", "Robert"];
+console.log(udaciFamily);
+
+const crew = ["Mal", "Zoe", "Wash", "Inara", "Jayne", "Kaylee"];
+console.log(crew);
+
+console.log(donuts.length);
+
+let longWord = "Pneumonoultramicroscopicsilicovolcanoconiosis";
+console.log(longWord.length);
+
+donuts.push("timbits");
+donuts.pop();
+donuts.push("timbits");
+console.log(donuts);
+
+donuts.splice(1, 1, "joe", "bill");
+console.log(donuts);
+
+let rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+rainbow.splice(2, 1, "Yellow", "Green");
+rainbow.splice(5, 0, "Purple");
+console.log(rainbow);
+
+let team = [
+  "Oliver Wood",
+  "Angelina Johnson",
+  "Katie Bell",
+  "Alicia Spinnet",
+  "George Weasley",
+  "Fred Weasley",
+  "Harry Potter",
+];
+
+function hasEnoughPlayers(players) {
+  if (players.length >= 7) {
+    return true;
+  } else return false;
+}
+console.log(hasEnoughPlayers(team));
+
+crew.push("Simon", "River", "Book");
+console.log(crew);
+
+crew.shift();
+console.log(crew);
+
+console.log(crew);
+for (let i = 0; i < donuts.length; i++) {
+  donuts[i] += "hole";
+  donuts[i] = donuts[i].toUpperCase();
+}
+console.log(donuts);
+
+let words = ["cat", "in", "hat"];
+words.forEach(function (word, num, all) {
+  console.log("Word " + num + " in " + all.toString() + " is " + word);
+});
+
+let test = [
+  12,
+  929,
+  11,
+  3,
+  199,
+  1000,
+  7,
+  1,
+  24,
+  37,
+  4,
+  19,
+  300,
+  3775,
+  299,
+  36,
+  209,
+  148,
+  169,
+  299,
+  6,
+  109,
+  20,
+  58,
+  139,
+  59,
+  3,
+  1,
+  139,
+];
+
+test.forEach(function (element, index) {
+  if (element % 3 === 0) {
+    test[index] += 100;
+  }
+  return test[index];
+});
+console.log(test);
+
+let improvedDonuts = donuts.map(function (donut) {
+  donut += " hole";
+  donut = donut.toUpperCase();
+  return donut;
+});
+console.log(donuts);
+
+var bills = [
+  50.23,
+  19.12,
+  34.01,
+  100.11,
+  12.15,
+  9.9,
+  29.11,
+  12.99,
+  10.0,
+  99.22,
+  102.2,
+  100.1,
+  6.77,
+  2.22,
+];
+
+let totals = bills.map(function (bill) {
+  bill += bill * 0.15;
+  bill = Number(bill.toFixed(2));
+  return bill;
+});
+
+console.log(totals);
+
+let donutBox = [
+  ["glazed", "chocolate glazed", "cinnamon"],
+  ["powdered", "sprinkled", "glazed cruller"],
+  ["chocolate cruller", "Boston creme", "creme de leche"],
+];
+
+for (let r = 0; r < donutBox.length; r++) {
+  for (let c = 0; c < donutBox[r].length; c++) {
+    console.log(donutBox[r][c]);
+  }
+}
+
+var numbers = [
+  [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+  [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+  [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+  [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+  [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+  [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+  [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+  [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+  [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+  [76, 7, 9, 6, 3, 73, 77, 100, 56, 100],
+];
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let x = 0; x < numbers[i].length; x++) {
+    if (numbers[i][x] % 2 === 0) {
+      numbers[i][x] = "even";
+    } else {
+      numbers[i][x] = "odd";
+    }
+  }
+}
+console.log(numbers);
+
+const director = "Steven Spielberg";
+const films = ["Jaws", "Indiana Jones", "A.I."];
+films[2] = "Jurassic Park";
+console.log(films);
+films.push("A.I");
+console.log(films);
+console.log(director + " is the director of " + films.length + " films");
+films.push("E.T");
+console.log(director + " is the director of " + films.length + " films");
+
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(iceCreamFlavours);
+
+const packingList = [
+  "bowls",
+  "plates",
+  "pots",
+  "pans",
+  "eating utensils",
+  "glasses",
+  "cups",
+  "cooking utensils",
+];
+
+console.log("Kitchen stuff to pack.");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+console.log(packingList[3]);
+
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
